b9cf4cf090fc3171b4c8153ad6a1f108b3f98fe4
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-02 22:51:12 +0800

    Create hari001.java

---
+++ hari001.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Ini print ");
+        System.out.print("Hellow ");
+        System.out.println("Halo dunia!");
+
+        String nama = "Isfanur Maysyarah";
+        int umur = 20;
+
+        System.out.printf("Nama: %s, Umur: %d\n", nama, umur);
+
+    }
+}
