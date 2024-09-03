93258028510fecb100d8b5e64db1d7129fc98145
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-03 20:01:31 +0800

    Create hari002.java

---
+++ hari002.java
@@ -0,0 +1,25 @@
+package hari2;
+
+public class  hari2{
+
+    public static void main(String[] args) {
+       boolean aktif = false;
+       char simbol = '#';
+       byte angkaKecil = 5;
+       short angkaSedang = 10;
+       int angkaBesar = 20;
+       long angkaSangatBesar = 50L;
+       float angkaDesimal = 3.14f;
+       double angkaDesimalBesar = 7.77;
+       
+        System.out.println(aktif);
+        System.out.println(simbol);
+        System.out.println(angkaKecil);
+        System.out.println(angkaSedang);
+        System.out.println(angkaBesar);
+        System.out.println(angkaSangatBesar);
+        System.out.println(angkaDesimal);
+        System.out.println(angkaDesimalBesar);
+    }
+    
+}
