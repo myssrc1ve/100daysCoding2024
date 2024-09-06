262ed15e0debd28d375816b3a6dc8e84dcf8a4e1
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-06 23:52:18 +0800

    Create hari005.java

---
+++ hari005.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class Komentar {
+
+    public static void main(String[] args) {
+
+        // Data
+        String nama = "Isfanur Maysyarah";
+        String Nim = "D0222318";
+        char kelas = 'A';
+        String jurusan = "Informatika";
+        int angkatan = 2022;
+
+        System.out.println("Nama = " + nama);
+        System.out.printf("NIM = %s \n", Nim); 
+        System.out.print("Jurusan = " + jurusan + "\n"); 
+        System.out.println("Kelas = " + kelas); 
+        System.out.println("Angkatan = " + angkatan); 
+    }
+
+}
