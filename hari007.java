5e7383b7a5d2a034c273335da9bb208352da712f
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-08 23:20:17 +0800

    Create hari007.java

---
+++ hari007.java
@@ -0,0 +1,13 @@
+public class hari007 {
+    public static void main(String[] args) {
+        // Inisialisasi variabel sederhana
+        int angka = 5;             // Angka integer
+        String nama = "Java";      // Teks
+        boolean aktif = true;      // Boolean
+      
+        // Menampilkan nilai variabel
+        System.out.println("Angka: " + angka);
+        System.out.println("Nama: " + nama);
+        System.out.println("Status aktif: " + aktif);
+    }
+}
