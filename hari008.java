9b416053ca897419a78b99aae2fadefda0e1dba4
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-09 22:09:25 +0800

    Create hari008.java

---
+++ hari008.java
@@ -0,0 +1,15 @@
+public class Update {
+    public static void main(String[] args) {
+        // Mendefinisikan nama barang awal
+        String namaBarang = "Lampu";
+
+        // Menampilkan nama barang sebelum update
+        System.out.println("Sebelum update: " + namaBarang);
+
+        // Mengupdate nama barang
+        namaBarang = "Lampu LED";
+
+        // Menampilkan nama barang setelah update
+        System.out.println("Setelah update: " + namaBarang);
+    }
+}
