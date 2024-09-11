f0a296945de2a8083fa2080703c9bb8dacba47db
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-11 23:25:33 +0800

    Create hari010.java

---
+++ hari010.java
@@ -0,0 +1,15 @@
+public class contohtipedatabyte {
+    public static void main(String[] args) {
+        // Deklarasi variabel tipe data byte
+        byte x = 10;
+        byte y = 20;
+
+        // Menjumlahkan dua byte
+        byte sum = (byte) (x + y);
+
+        // Menampilkan hasil
+        System.out.println("Nilai x: " + x);
+        System.out.println("Nilai y: " + y);
+        System.out.println("Hasil penjumlahan x + y: " + sum);
+    }
+}
