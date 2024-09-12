b2a34f7714851b0594d1e3d55c22add3e7c7fa92
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-12 21:24:13 +0800

    Create hari011.java

---
+++ hari011.java
@@ -0,0 +1,10 @@
+public class TugasShort {
+    public static void main(String[] args) {
+        // Deklarasi variabel tipe data short
+        short jarak = 1500; // Jarak dalam meter
+        short tahunLahir = 2003; // Tahun lahir
+
+        System.out.println("Jarak: " + jarak + " meter");
+        System.out.println("Tahun Lahir: " + tahunLahir);
+    }
+}
