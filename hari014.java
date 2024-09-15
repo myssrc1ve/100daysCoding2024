4a09e8212633c865aaae3511c209951e40729faf
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-15 21:37:12 +0800

    Create hari014.java

---
+++ hari014.java
@@ -0,0 +1,9 @@
+public class ContohLong {
+    public static void main(String[] args) {
+        long tinggiGunungEverest = 8848000L; // Tinggi Gunung Everest dalam meter
+        long kecepatanCahaya = 299792458L; // Kecepatan cahaya dalam meter per detik
+
+        System.out.println("Tinggi Gunung Everest adalah: " + tinggiGunungEverest + " meter.");
+        System.out.println("Kecepatan cahaya adalah: " + kecepatanCahaya + " meter per detik.");
+    }
+}
