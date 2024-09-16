ab9edae5a98754bbdf1aa987ac6ba2ed89825fa7
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-16 21:17:02 +0800

    Create hari015.java

---
+++ hari015.java
@@ -0,0 +1,9 @@
+public class contohDouble {
+    public static void main(String[] args) {
+        // Deklarasi variabel tipe data double
+        double nilai = 12.34;
+
+        // Menampilkan nilai dari variabel
+        System.out.println("Nilai double: " + nilai);
+    }
+}
