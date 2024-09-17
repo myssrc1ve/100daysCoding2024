c7c4384043b47de8d7e194bc61def8dc2e6ce3b2
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-17 22:46:11 +0800

    Create hari017.java

---
+++ hari017.java
@@ -0,0 +1,11 @@
+public class contohFloat {
+    public static void main(String[] args) {
+        
+        float pi = 3.14f; 
+        float radius = 5.5f;
+        float area = pi * radius * radius;
+
+        
+        System.out.println("Luas lingkaran dengan radius " + radius + " adalah " + area);
+    }
+}
