b0bea31d6afc3275a57494dd63c86158fb5ca279
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-18 19:52:33 +0800

    Create hari018.java

---
+++ hari018.java
@@ -0,0 +1,16 @@
+public class ContohChar {
+    public static void main(String[] args) {
+        
+        char initial = 'A'; 
+        char grade = 'B';
+
+        System.out.println("Inisial: " + initial);
+        System.out.println("Nilai: " + grade);
+
+        int asciiInitial = initial;
+        int asciiGrade = grade;
+
+        System.out.println("Nilai ASCII dari " + initial + " adalah: " + asciiInitial);
+        System.out.println("Nilai ASCII dari " + grade + " adalah: " + asciiGrade);
+    }
+}
