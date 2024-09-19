ee77862a33f1d9aea5a36437ca4b129714e801e1
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-19 21:04:31 +0800

    Create hari018(ori).java

---
+++ hari018(ori).java
@@ -0,0 +1,20 @@
+public class ContohBoolean {
+    public static void main(String[] args) {
+
+        boolean isRaining = false; 
+        
+        if (isRaining) {
+            System.out.println("Bawa payung!");
+        } else {
+            System.out.println("Cuaca cerah, tidak perlu payung.");
+        }
+
+        isRaining = true; 
+        
+        if (isRaining) {
+            System.out.println("Bawa payung!");
+        } else {
+            System.out.println("Cuaca cerah, tidak perlu payung.");
+        }
+    }
+}
