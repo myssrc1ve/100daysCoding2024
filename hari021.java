e7197c05edfcfe9ff9f90cb0a40db8d707b9c620
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-22 21:05:21 +0800

    Create hari021.java

---
+++ hari021.java
@@ -0,0 +1,10 @@
+public class ContohShort {
+    public static void main(String[] args) {
+                Short angka = 10; 
+
+                System.out.println("Nilai dari objek Short: " + angka);
+        
+             Short hasil = (short) (angka + 5); 
+        System.out.println("Nilai setelah ditambah 5: " + hasil);
+    }
+}
