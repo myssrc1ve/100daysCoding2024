d20dd65e70699dac06d5ce98b5a5c720e3b38986
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-09-26 22:35:36 +0800

    Create hari025.java

---
+++ hari025.java
@@ -0,0 +1,13 @@
+public class referensiDouble {
+    public static void main(String[] args) {
+        
+        Double num1 = new Double(8.2);
+        Double num2 = new Double(3.8);
+      
+        System.out.println("Nilai num1: " + num1);
+        System.out.println("Nilai num2: " + num2);
+
+        Double average = (num1 + num2) / 2;
+        System.out.println("Rata-rata: " + average);
+    }
+}
