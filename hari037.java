2bd3ae4fd8e3ebf007c12492d2cc9bfcea3b6abb
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-08 20:14:09 +0800

    Create hari037.java

---
+++ hari037.java
@@ -0,0 +1,15 @@
+public class OperatorPenugasan {
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.println("Nilai a: " + a);
+
+        int b = 5;
+        b += 3; 
+        System.out.println("Nilai b setelah penambahan: " + b);
+
+        int c = 20;
+        c -= 4; 
+        System.out.println("Nilai c setelah pengurangan: " + c);
+    }
+}
