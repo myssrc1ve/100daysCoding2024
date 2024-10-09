1312724f0532cf549a90f2cf6a94ed3261f1ab43
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-09 20:21:47 +0800

    Create hari038.java

---
+++ hari038.java
@@ -0,0 +1,16 @@
+public class OperatorPenugasan {
+    public static void main(String[] args) {
+      
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Nilai awal a: " + a);
+        System.out.println("Nilai awal b: " + b);
+
+        a *= b; 
+        System.out.println("Nilai a setelah perkalian: " + a);
+
+        b /= 2; 
+        System.out.println("Nilai b setelah pembagian: " + b);
+    }
+}
