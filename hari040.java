ad42b3de200cea52f21f53cf19c77f1e2b0de21e
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-11 23:05:38 +0800

    Create hari040.java

---
+++ hari040.java
@@ -0,0 +1,18 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println(a + " lebih besar dari " + b);
+        } else {
+            System.out.println(a + " tidak lebih besar dari " + b);
+        }
+
+        if (a < b) {
+            System.out.println(a + " lebih kecil dari " + b);
+        } else {
+            System.out.println(a + " tidak lebih kecil dari " + b);
+        }
+    }
+}
