d20400f1c9b173615333ddc187e99678ad183a5b
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-12 23:06:26 +0800

    Create hari041(2).java

---
+++ hari041(2).java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+
+        System.out.println(b);
+
+        b = 5;
+        System.out.println(b);
+
+        b = 75;
+        System.out.println(b);
+
+        b = 60;
+        System.out.println(b);
+
+        b = 4;
+        System.out.println(b);
+
+        b = 19;
+        System.out.println(b);
+    }
+}
