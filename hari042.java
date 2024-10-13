26cbf33d876e7e330f8db381b0a7f36fd248dc18
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-13 18:45:47 +0800

    Create hari042.java

---
+++ hari042.java
@@ -0,0 +1,18 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a == b) {
+            System.out.println("a sama dengan b");
+        } else {
+            System.out.println("a tidak sama dengan b");
+        }
+
+        if (a != b) {
+            System.out.println("a tidak sama dengan b");
+        } else {
+            System.out.println("a sama dengan b");
+        }
+    }
+}
