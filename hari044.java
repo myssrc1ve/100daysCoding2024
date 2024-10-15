803524c97964f4dd9f952d04406f161fa0598fa7
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-15 13:43:20 +0800

    Create hari044.java

---
+++ hari044.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        boolean kondisi1 = true;
+        boolean kondisi2 = false;
+
+        if (kondisi1 && kondisi2) {
+            System.out.println("Kedua kondisi benar.");
+        } else {
+            System.out.println("Salah satu atau kedua kondisi salah.");
+        }
+    }
+}
