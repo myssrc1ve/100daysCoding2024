d4c72f982160e4370f48c6d5a9563114b0ba308d
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-16 20:44:50 +0800

    Create hari045.java

---
+++ hari045.java
@@ -0,0 +1,12 @@
+public class OperatorLogikaOR {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        if (a || b) {
+            System.out.println("Salah satu dari a atau b adalah true.");
+        } else {
+            System.out.println("Keduanya adalah false.");
+        }
+    }
+}
