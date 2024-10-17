c603fa1f0c8584e0b587638c4cec513ee9b4bc06
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-17 23:11:55 +0800

    Create hari046.java

---
+++ hari046.java
@@ -0,0 +1,11 @@
+public class NegasiExample {
+    public static void main(String[] args) {
+        boolean nilai = true;
+        
+        System.out.println("Nilai awal: " + nilai);
+        
+        boolean kebalikan = !nilai;
+        
+        System.out.println("Kebalikan nilai: " + kebalikan);
+    }
+}
