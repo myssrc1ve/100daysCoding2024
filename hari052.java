03b86c34d66c967d3478e1af05de4cd1605c9532
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-23 12:48:51 +0800

    Create hari052.java

---
+++ hari052.java
@@ -0,0 +1,11 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Menggunakan operator ternary untuk menentukan nilai yang lebih besar
+        int max = (a > b) ? a : b;
+
+        System.out.println("Nilai maksimum adalah: " + max);
+    }
+}
