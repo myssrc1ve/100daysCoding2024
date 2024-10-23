92d8b46b4b03de3e292eded1239ff1274a366a40
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-23 12:49:22 +0800

    Update hari052.java

--- hari052.java
+++ hari052.java
@@ -3,7 +3,6 @@ public class Ternary {
         int a = 10;
         int b = 20;
 
-        // Menggunakan operator ternary untuk menentukan nilai yang lebih besar
         int max = (a > b) ? a : b;
 
         System.out.println("Nilai maksimum adalah: " + max);
