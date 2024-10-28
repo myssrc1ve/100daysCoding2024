479f430da6efc3c87f07e7c75b8f538ccf2b99ed
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-28 19:22:37 +0800

    Create hari057.java

---
+++ hari057.java
@@ -0,0 +1,11 @@
+public class LoopWhileExample {
+    public static void main(String[] args) {
+        int i = 1; // Inisialisasi variabel
+
+        // Looping while
+        while (i <= 10) { // Kondisi looping
+            System.out.println("Nilai i: " + i); // Menampilkan nilai i
+            i++; // Increment nilai i
+        }
+    }
+}
