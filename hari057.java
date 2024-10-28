cabe7e172c0e85829981c2553eecc70800d1297b
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-28 19:23:31 +0800

    Update hari057.java

--- hari057.java
+++ hari057.java
@@ -1,11 +1,10 @@
 public class LoopWhileExample {
     public static void main(String[] args) {
-        int i = 1; // Inisialisasi variabel
+        int i = 1
 
-        // Looping while
-        while (i <= 10) { // Kondisi looping
+        while (i <= 10) {
             System.out.println("Nilai i: " + i); // Menampilkan nilai i
-            i++; // Increment nilai i
+            i++;
         }
     }
 }
