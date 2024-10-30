7004c15c2042e3f8f2cd4074413d7f01a654c93c
myssrc1ve <152672079+myssrc1ve@users.noreply.github.com>
2024-10-30 20:36:06 +0800

    Create hari059.java

---
+++ hari059.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apel", "Jeruk", "Pisang", "Mangga"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
